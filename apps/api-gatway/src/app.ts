e85cd5d4732f97044bd7d384176e9560e7178907
semaphore <devteam@semaphoremobile.com>
2026-01-12 15:04:41 +0530

    authorization

--- apps/api-gatway/src/app.ts
+++ apps/api-gatway/src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import authMiddleware from './middlewares/auth.middlewares';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/health', (req, res) => {
+app.get('/health', authMiddleware, (req, res) => {
   res.json({ status: 'API Gateway is running' });
 });
 
