6c4323c7d318e4b97fcc5ba1005f6e798f08b303
semaphore <devteam@semaphoremobile.com>
2026-01-13 20:03:12 +0530

    user service db connection

--- apps/auth-service/src/index.ts
+++ apps/auth-service/src/index.ts
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();    
 import app from './app';
-import { connectNATS } from './utils/nats';
 import { connectDB } from './database/mongo';
+import { connectNATS } from './utils/nats';
 
 const port = process.env.PORT || 4000;
 
 const start = async () => {
+  console.log('calling index')
   await connectDB();  
   await connectNATS()
   app.listen(port, () => {
